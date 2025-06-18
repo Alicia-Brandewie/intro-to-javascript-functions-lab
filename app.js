23467c0665f14890b5f62a3bbddd9c87e9ec4820
alicia.brandewie@gmail.com <alicia@DESKTOP-BFU1FC6.localdomain>
2025-06-18 11:59:12 -0500

    complete exercise one, two, three, and four

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ Example: isCharAVowel('a') should return true.
 
 Complete the exercise in the space below:
 */
+
 function isCharAVowel(character){
 if (character === "a" || "e" || "i" || "o" || "u" ) {
     return "true";
@@ -60,3 +61,22 @@ if (character === "a" || "e" || "i" || "o" || "u" ) {
 }
 
 console.log('Exercise 3 Result:', isCharAVowel("a")); 
+
+/*
+Exercise 4: generateEmail()
+
+Create a function named generateEmail. It should take two strings: 
+a name and a domain. It should return a simple email address.
+
+Example: generateEmail('johnsmith', 'example.com') 
+should return 'johnsmith@example.com'.
+
+Complete the exercise in the space below:
+*/
+
+function generateEmail(name, domain) {
+    return (name +"@"+domain)
+}
+
+
+console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
